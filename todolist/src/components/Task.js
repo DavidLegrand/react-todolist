d17e55e3b334db7e35d343cf3116c1a83cb3e9e5
David <DavidLegrand@users.noreply.github.com>
2020-11-16 18:46:59 +0100

    Buttons added

--- todolist/src/components/Task.js
+++ todolist/src/components/Task.js
@@ -3,23 +3,27 @@ import PropTypes from 'prop-types'
 import { Badge, Button, ListGroupItem } from 'react-bootstrap'
 import TaskModel from '../models/task'
 
-const Task = ({ task, cancel, complete }) => {
+
+const Task = () => {
+  const task = new TaskModel(1, "Finaliser l'API todolist", false)
+
   const getCompleted = () => task.completed ? "Terminée" : "En cours"
   const getColor = () => task.completed ? "success" : "warning"
+  const getButtonColor = () => task.completed ? "dark" : "success"
+
   return (
     <ListGroupItem variant={getColor()}>
       <h2 className="h4">
         {task.title}
         <Badge className="float-right" variant={getColor()}>{getCompleted()}</Badge>
       </h2>
+      {task.completed ?
+        <Button onClick={() => console.log(task, "tâche annulée")} variant={getButtonColor()}>Annuler</Button> :
+        <Button onClick={() => console.log(task, "tâche terminée")} variant={getButtonColor()}>Terminer</Button>
+      }
     </ListGroupItem>
   )
 }
 
-Task.propTypes = {
-  task: PropTypes.instanceOf(TaskModel).isRequired,
-  cancel: PropTypes.func.isRequired,
-  complete: PropTypes.func.isRequired,
-}
 
 export default Task
