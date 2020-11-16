a8adde8b4060451b93b4823e81d0f28ac1a339fe
David <DavidLegrand@users.noreply.github.com>
2020-11-16 12:16:28 +0100

    Ajout des components Task et ToDoList

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Row, Col, Container } from 'react-bootstrap'
+import ToDoList from './components/ToDoList';
 function App() {
   return (
     <Container>
       <Row>
         <Col>
           <h1>To Do List</h1>
+          <ToDoList />
         </Col>
       </Row>
     </Container>
---
+++ todolist/src/components/Task.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ListGroupItem } from 'react-bootstrap'
+
+const Task = () => {
+  return (
+    <ListGroupItem>Ceci est une tâche</ListGroupItem>
+  )
+}
+
+export default Task
