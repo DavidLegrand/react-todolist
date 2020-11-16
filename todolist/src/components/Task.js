6a030e7ae3acfc6ecf03c954f7100e75ec7a1082
David <DavidLegrand@users.noreply.github.com>
2020-11-16 18:44:36 +0100

    Removed Buttons

--- todolist/src/components/Task.js
+++ todolist/src/components/Task.js
@@ -6,17 +6,12 @@ import TaskModel from '../models/task'
 const Task = ({ task, cancel, complete }) => {
   const getCompleted = () => task.completed ? "Terminée" : "En cours"
   const getColor = () => task.completed ? "success" : "warning"
-  const getButtonColor = () => task.completed ? "dark" : "success"
   return (
     <ListGroupItem variant={getColor()}>
       <h2 className="h4">
         {task.title}
         <Badge className="float-right" variant={getColor()}>{getCompleted()}</Badge>
       </h2>
-      {task.completed ?
-        <Button onClick={() => cancel(task)} variant={getButtonColor()}>Annuler</Button> :
-        <Button onClick={() => complete(task)} variant={getButtonColor()}>Terminer</Button>
-      }
     </ListGroupItem>
   )
 }
