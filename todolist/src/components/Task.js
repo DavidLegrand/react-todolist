e4345dccfb76b148232e5fb862b48927eab7b826
David <DavidLegrand@users.noreply.github.com>
2020-11-16 16:56:49 +0100

    Tasks transmited from ToDoList to Task via props

--- todolist/src/components/Task.js
+++ todolist/src/components/Task.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Badge, ListGroupItem } from 'react-bootstrap'
+import PropTypes from 'prop-types'
+import { Badge, Button, ListGroupItem } from 'react-bootstrap'
 import TaskModel from '../models/task'
 
-const Task = () => {
-  const task = new TaskModel(1, "Finaliser l'API todolist", false)
-
-
+const Task = ({ task, cancel, complete }) => {
 
   return (
     <ListGroupItem variant={task.getColor()}>
@@ -13,8 +11,18 @@ const Task = () => {
         {task.title}
         <Badge className="float-right" variant={task.getColor()}>{task.getCompleted()}</Badge>
       </h2>
+      {task.completed ?
+        <Button onClick={() => cancel(task)} variant={task.getCancelColor()}>Annuler</Button> :
+        <Button onClick={() => complete(task)} variant={task.getCancelColor()}>Terminer</Button>
+      }
     </ListGroupItem>
   )
+}
+
+Task.propTypes = {
+  task: PropTypes.instanceOf(TaskModel).isRequired,
+  cancel: PropTypes.func.isRequired,
+  complete: PropTypes.func.isRequired,
 }
 
 export default Task
--- todolist/src/components/ToDoList.js
+++ todolist/src/components/ToDoList.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { ListGroup } from 'react-bootstrap'
+import TaskModel from '../models/task'
 import Task from './Task'
 
 const ToDoList = () => {
+  const task1 = new TaskModel(1, "Finaliser les maquettes", true)
+  const task2 = new TaskModel(2, "Terminer l'API backend", false)
+  const task3 = new TaskModel(3, "Intégrer le formulaire", false)
+
+
+  const complete = (t) => { console.log(t) }
+  const cancel = (t) => { console.log(t) }
+
   return (
     <ListGroup>
-      <Task />
-      <Task />
-      <Task />
+      <Task task={task1} complete={complete} cancel={cancel} />
+      <Task task={task2} complete={complete} cancel={cancel} />
+      <Task task={task3} complete={complete} cancel={cancel} />
     </ListGroup>
   )
 }
