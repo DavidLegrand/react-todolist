8e6ff5c22ecc8717da7db7610370b9073d93d9aa
David <DavidLegrand@users.noreply.github.com>
2020-11-16 18:42:32 +0100

    moved model methods to component task

--- todolist/src/components/Task.js
+++ todolist/src/components/Task.js
@@ -5,17 +5,19 @@ import TaskModel from '../models/task'
 const Task = () => {
   const task = new TaskModel(1, "Finaliser l'API todolist", false)
 
-
+  const getCompleted = () => task.completed ? "Terminée" : "En cours"
+  const getColor = () => task.completed ? "success" : "warning"
+  const getButtonColor = () => task.completed ? "dark" : "success"
 
   return (
-    <ListGroupItem variant={task.getColor()}>
+    <ListGroupItem variant={getColor()}>
       <h2 className="h4">
         {task.title}
-        <Badge className="float-right" variant={task.getColor()}>{task.getCompleted()}</Badge>
+        <Badge className="float-right" variant={getColor()}>{getCompleted()}</Badge>
       </h2>
       {task.completed ?
-        <Button onClick={() => console.log(task)} variant={task.getButtonColor()}>Annuler</Button> :
-        <Button onClick={() => console.log(task)} variant={task.getButtonColor()}>Terminer</Button>
+        <Button onClick={() => console.log(task)} variant={getButtonColor()}>Annuler</Button> :
+        <Button onClick={() => console.log(task)} variant={getButtonColor()}>Terminer</Button>
       }
     </ListGroupItem>
   )
