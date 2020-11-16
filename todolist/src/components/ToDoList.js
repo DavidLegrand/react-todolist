7ae63f0fbc3653543cd272a62cf0fb1860c79f86
David <DavidLegrand@users.noreply.github.com>
2020-11-16 18:37:34 +0100

    State managment in todolist included

--- todolist/src/components/ToDoList.js
+++ todolist/src/components/ToDoList.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ListGroup } from 'react-bootstrap'
 import TaskModel from '../models/task'
 import Task from './Task'
 
 const ToDoList = () => {
-  const list = [
+  const [list, setlist] = useState([
     new TaskModel(1, "Finaliser les maquettes", true),
     new TaskModel(2, "Terminer l'API backend", false),
     new TaskModel(3, "Intégrer le formulaire", false),
-  ]
+  ])
 
-  const complete = (t) => { console.log(t) }
-  const cancel = (t) => { console.log(t) }
+  const complete = (t) => updateTask(t, true)
+  const cancel = (t) => updateTask(t, false)
+  
+  const updateTask = (t, completed) => {
+    const newTask = new TaskModel();
+    const newList = (list.map(task =>
+      task.id === t.id ?
+        Object.assign(newTask, { ...task, completed: completed }) :
+        task))
+    setlist(newList)
+  }
 
   return (
     <ListGroup>
--- todolist/src/models/task.js
+++ todolist/src/models/task.js
@@ -2,7 +2,5 @@ export default class TaskModel {
   constructor(id, title, completed = false, userId = null) {
     this.id = id; this.title = title; this.completed = completed; this.userId = userId
   }
-  getCompleted = () => this.completed ? "Terminée" : "En cours"
-  getColor = () => this.completed ? "success" : "warning"
-  getButtonColor = () => this.completed ? "dark" : "success"
+ 
 }
