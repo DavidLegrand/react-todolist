1d9bbeea81459f1cb4ac332d6198fbd181a15bde
David <DavidLegrand@users.noreply.github.com>
2020-11-16 18:51:32 +0100

    Added the buttons

--- todolist/src/components/Task.js
+++ todolist/src/components/Task.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Badge, ListGroupItem } from 'react-bootstrap'
+import { Badge, ListGroupItem, Button } from 'react-bootstrap'
 import TaskModel from '../models/task'
 
 const Task = ({ task, cancel, complete }) => {
+
   const getCompleted = () => task.completed ? "Terminée" : "En cours"
   const getColor = () => task.completed ? "success" : "warning"
+  const getButtonColor = () => task.completed ? "dark" : "success"
+
   return (
     <ListGroupItem variant={getColor()}>
       <h2 className="h4">
         {task.title}
         <Badge className="float-right" variant={getColor()}>{getCompleted()}</Badge>
       </h2>
+      {task.completed ?
+        <Button onClick={() => cancel()} variant={getButtonColor()}>Annuler</Button> :
+        <Button onClick={() => complete()} variant={getButtonColor()}>Terminer</Button>
+      }
     </ListGroupItem>
   )
 }
