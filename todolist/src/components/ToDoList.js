a4ca175754d6b2548587ac45d12cba113770a62f
David <DavidLegrand@users.noreply.github.com>
2020-11-21 16:50:19 +0100

    Fetch is now complete and working

--- todolist/src/components/ToDoList.js
+++ todolist/src/components/ToDoList.js
@@ -5,11 +5,45 @@ import Task from './Task'
 import NewTaskForm from './NewTaskForm'
 
 const ToDoList = () => {
-  const [list, setlist] = useState([
-    new TaskModel(1, "Finaliser les foutues maquettes", true),
-    new TaskModel(2, "Terminer l'API backend", false),
-    new TaskModel(3, "Intégrer le formulaire", false),
-  ])
+  const [list, setlist] = useState([])
+
+  const [loading, setloading] = useState(true)
+
+
+  const endpoint = `https://todolist-react-7495e.firebaseio.com/tasks.json`
+
+  useEffect(() => {
+    const sendData = () => {
+      const options = {
+        method: 'PUT',
+        mode: 'cors',
+        body: JSON.stringify(list),
+        headers: { 'Content-Type': 'application/json' },
+      }
+      fetch(endpoint, options)
+        .then(response => { if (response.ok) console.log('data sent to server') })
+        .catch(error => console.error(error))
+    }
+    if (list.length > 0 && list !== null)
+      sendData()
+  }, [list, endpoint])
+
+  useEffect(() => {
+    const getData = () => {
+      fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+          setloading(false)
+          setlist(
+            data.map(task =>
+              Object.assign(new TaskModel(), task))
+          )
+        })
+        .catch(error => console.error(error))
+    }
+    getData()
+  }, [endpoint])
+
 
   const complete = (t) => updateCompleted(t, true)
   const cancel = (t) => updateCompleted(t, false)
@@ -29,14 +63,17 @@ const ToDoList = () => {
   }
 
   const getLastId = () => list.reduce((prev, curr) => prev.id > curr.id ? prev : curr).id
-  
+
   const addTask = (task) => {
-    setlist([...list, Object.assign(new TaskModel(), { ...task, id: getLastId() + 1 })])
+    setlist([...list, Object.assign(new TaskModel(), { ...task, created: new Date(), id: getLastId() + 1 })])
   }
   return (
     <>
       <ListGroup>
-        {list.map(t => <Task task={t} key={t.id} complete={complete} cancel={cancel} />)}
+        {loading ?
+          <ListGroupItem variant="light" className="text-center">"... Chargement"</ListGroupItem> :
+          list.map(t => <Task task={t} key={t.id} complete={complete} cancel={cancel} />)
+        }
         <ListGroupItem variant="light" className="text-center">
           <Button onClick={() => cancelAll()} variant="dark" >Tout annuler</Button>
           <Button onClick={() => completeAll()} variant="success">Tout terminer</Button>
