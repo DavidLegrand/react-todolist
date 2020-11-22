d5a62944504cacda548dbc4845523513673f8319
David <DavidLegrand@users.noreply.github.com>
2020-11-23 00:45:52 +0100

    Custom useFetch and usePut hooks

--- todolist/src/components/ToDoList.js
+++ todolist/src/components/ToDoList.js
@@ -4,54 +4,37 @@ import TaskModel from '../models/task'
 import Task from './Task'
 import NewTaskForm from './NewTaskForm'
 import { UserIdContext } from '../context'
-import { usePutList, useGetList } from '../hooks'
+import { useFetch, usePut } from '../hooks'
 
 const ToDoList = () => {
-  const [list, setlist] = useState(null)
+
+  const [list, setlist] = useState([])
+
   const [userId, setuserId] = useContext(UserIdContext)
   const endpoint = `https://todolist-react-7495e.firebaseio.com/tasks.json`
 
-
-  useGetList(endpoint, list, setlist, TaskModel)
-  //usePutList(endpoint, list)
-
+  const fetch = useFetch(endpoint, setlist)
+  usePut(endpoint, list)
 
   const complete = (t) => updateCompleted(t, true)
   const cancel = (t) => updateCompleted(t, false)
   const completeAll = () => updateCompletedAll(true)
   const cancelAll = () => updateCompletedAll(false)
 
-  const updateCompleted = (t, isComplete) => {
-    setlist(list.map(task =>
-      task.id === t.id ?
-        Object.assign(new TaskModel(), { ...task, completed: isComplete }) :
-        task))
-  }
+  const updateCompleted = (t, isComplete) => { setlist(list.map(task => task.id === t.id ? { ...task, completed: isComplete } : task)) }
+  const updateCompletedAll = (isComplete) => { setlist(list.map(task => { return { ...task, completed: isComplete } })) }
 
-  const updateCompletedAll = (isComplete) => {
-    setlist(list.map(task =>
-      Object.assign(new TaskModel(), { ...task, completed: isComplete })))
-  }
-
-  const getLastId = () => list.reduce((prev, curr) => prev.id > curr.id ? prev : curr).id
-
-  const addTask = (task) => {
-    setlist([...list, Object.assign(new TaskModel(), {
-      ...task,
-      created: new Date(),
-      id: getLastId() + 1,
-      userId: userId
-    })])
-  }
+  const getLastId = () => list.reduce((prev, curr) => prev.id > curr.id ? prev : curr).id$
+  const addTask = (task) => { setlist([...list, {...task, created: new Date(), id: getLastId() + 1, userId: userId}])}
 
   return (
     <>
       User Id : <input type="number" value={userId} onChange={(e) => setuserId(+e.target.value)} />
       <ListGroup>
         {
-          !list ?
+          fetch.isLoading ?
             <ListGroupItem variant="light" className="text-center">... Chargement</ListGroupItem> :
-            list.map(t => t.userId === userId && <Task task={t} key={t.id} complete={complete} cancel={cancel} />)
+            list.map(t => t.userId === userId && <Task task={Object.assign(new TaskModel(), t)} key={t.id} complete={complete} cancel={cancel} />)
         }
         <ListGroupItem variant="light" className="text-center">
           <Button onClick={() => cancelAll()} variant="dark" >Tout annuler</Button>
--- todolist/src/hooks/index.js
+++ todolist/src/hooks/index.js
@@ -1,73 +1,59 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
-// useEffect(() => {
-//   const sendData = () => {
-//     const options = {
-//       method: 'PUT',
-//       mode: 'cors',
-//       body: JSON.stringify(list),
-//       headers: { 'Content-Type': 'application/json' },
-//     }
-//     fetch(endpoint, options)
-//       .then(response => { if (response.ok) console.log('data sent to server'); return response.json() })
-//       .catch(error => console.error(error))
-//   }
-//   if (loading === false && list.length > 0)
-//     sendData()
-// }, [list, endpoint])
-
-const usePutList = (endpoint, list) => {
+const usePut = async (endpoint, data) => {
   const options = {
     method: 'PUT',
     mode: 'cors',
-    body: JSON.stringify(list),
+    body: JSON.stringify(data),
     headers: { 'Content-Type': 'application/json' },
   }
 
-  useFetch(endpoint, options)
-    .then(res => { console.log("LIST from PUT", list); console.log('data sent to server', res, res.ok) })
-}
-
-const useGetList = (endpoint, list, setlist, Model = null) => {
-  useFetch(endpoint, {})
-    .then(res => {
-      if (!list)
-        setlist(res.data.map(e => Model ? Object.assign(new Model(), e) : e))
-      console.log("LIST from GET", list)
-    })
-    .catch(error => console.error(error))
-}
-
-const useFetch = async (endpoint, options = {}) => {
-
-  const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const result = { data, error, isLoading }
 
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      /* const res = await fetch(endpoint, options);
+      const json = await res.json();*/
+      console.log("data sent", data)
+      setIsLoading(false)
+    } catch (error) {
+      setError(error);
+    }
+  };
   useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        if (!data) {
-          const res = await fetch(endpoint, options);
-          const json = await res.json();
-          setData(json);
-          setIsLoading(false)
-          console.log("on entre ici", json)
-        }
+    fetchData();
+  }, [data]);
 
-      } catch (error) {
-        setError(error);
-      }
-    };
+  return { error, isLoading };
+}
+
+
+
+const useFetch = async (endpoint, setData) => {
+
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(endpoint);
+      const json = await res.json();
+      setData(json);
+      setIsLoading(false)
+      if (res.ok) { console.log("data fetched", json) }
+    } catch (error) {
+      setError(error);
+    }
+  };
+  useEffect(() => {
     fetchData();
   }, []);
-  return new Promise((resolve, reject) => {
-    if (error) reject(error)
-    else if (!isLoading && data) resolve(result)
-  });
+
+  return { error, isLoading };
 };
 
 
-export { useFetch, useGetList, usePutList }
+export { useFetch, usePut }
