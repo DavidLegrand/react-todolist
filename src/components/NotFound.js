99fac23364fb3b830cef9e80adf237bc2cde23f3
David <DavidLegrand@users.noreply.github.com>
2020-11-24 09:21:32 +0100

    Added Not Found Component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Title from './shared/Title'
+import {Link} from 'react-router-dom'
 
 const NotFound = () => {
   return (
@@ -7,6 +8,7 @@ const NotFound = () => {
       <Title>Oh oh !</Title>
       <p className="display-4 text-center">404 Not Found :(</p>
       <p className="text-center">La page demandée n'existe pas.</p>
+      <p className="text-center"><Link to='/'>Retour à l'accueil</Link></p>
     </>
   )
 }
--- src/components/ToDoList/TaskDetails.js
+++ src/components/ToDoList/TaskDetails.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Redirect } from 'react-router-dom'
 import { Table } from 'react-bootstrap'
 import { useFetch } from '../../hooks'
 import Title from '../shared/Title'
+import NotFound from '../NotFound'
 
 const TaskDetails = () => {
   const { id } = useParams()
@@ -14,17 +15,22 @@ const TaskDetails = () => {
       {fetch.isLoading ?
         "...Chargement" :
         <>
-          <Title>{task.title}</Title>
-          <Table striped bordered hover>
-            <tbody>
-              <tr><td>ID</td><td>{task.id}</td></tr>
-              <tr><td>Statut</td><td>{task.completed ? "Terminée" : "En cours"}</td></tr>
-              <tr><td>Créée le</td><td>{new Date(task.created).toLocaleDateString()}</td></tr>
-              <tr><td>A</td><td>{new Date(task.created).toLocaleTimeString()}</td></tr>
-              <tr><td>Description</td><td>{task.description}</td></tr>
-              <tr><td>Utilisateur</td><td>{task.userId}</td></tr>
-            </tbody>
-          </Table>
+          {task === null ?
+            <NotFound /> :
+            <>
+              <Title>{task.title}</Title>
+              <Table striped bordered hover>
+                <tbody>
+                  <tr><td>ID</td><td>{task.id}</td></tr>
+                  <tr><td>Statut</td><td>{task.completed ? "Terminée" : "En cours"}</td></tr>
+                  <tr><td>Créée le</td><td>{new Date(task.created).toLocaleDateString()}</td></tr>
+                  <tr><td>A</td><td>{new Date(task.created).toLocaleTimeString()}</td></tr>
+                  <tr><td>Description</td><td>{task.description}</td></tr>
+                  <tr><td>Utilisateur</td><td>{task.userId}</td></tr>
+                </tbody>
+              </Table>
+            </>
+          }
         </>
       }
     </>
