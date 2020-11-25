ea086c66616a204ef0287128bf3397c1b590ecef
David <DavidLegrand@users.noreply.github.com>
2020-11-25 13:51:36 +0100

    Repaired fetch task for one task only

--- src/components/ToDoList/TaskDetails.js
+++ src/components/ToDoList/TaskDetails.js
@@ -8,25 +8,25 @@ import NotFound from '../NotFound'
 const TaskDetails = () => {
   const { id } = useParams()
   const [task, settask] = useState({})
-  const fetch = useFetch(`https://todolist-react-7495e.firebaseio.com/tasks/${id}.json`, settask)
+  const fetch = useFetch(`https://todolist-react-7495e.firebaseio.com/tasks.json?orderBy="id"&equalTo=${id}`, settask)
 
   return (
     <>
       {fetch.isLoading ?
         "...Chargement" :
         <>
-          {task === null ?
+          {task[0] === null ?
             <NotFound /> :
             <>
-              <Title>{task.title}</Title>
+              <Title>{task[0].title}</Title>
               <Table striped bordered hover>
                 <tbody>
-                  <tr><td>ID</td><td>{task.id}</td></tr>
-                  <tr><td>Statut</td><td>{task.completed ? "Terminée" : "En cours"}</td></tr>
-                  <tr><td>Créée le</td><td>{new Date(task.created).toLocaleDateString()}</td></tr>
-                  <tr><td>A</td><td>{new Date(task.created).toLocaleTimeString()}</td></tr>
-                  <tr><td>Description</td><td>{task.description}</td></tr>
-                  <tr><td>Utilisateur</td><td>{task.userId}</td></tr>
+                  <tr><td>ID</td><td>{task[0].id}</td></tr>
+                  <tr><td>Statut</td><td>{task[0].completed ? "Terminée" : "En cours"}</td></tr>
+                  <tr><td>Créée le</td><td>{new Date(task[0].created).toLocaleDateString()}</td></tr>
+                  <tr><td>A</td><td>{new Date(task[0].created).toLocaleTimeString()}</td></tr>
+                  <tr><td>Description</td><td>{task[0].description}</td></tr>
+                  <tr><td>Utilisateur</td><td>{task[0].userId}</td></tr>
                 </tbody>
               </Table>
             </>
